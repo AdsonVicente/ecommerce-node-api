4e3fd9aac75612d2f0184c249408464d410d3fb9
Adson Vicente <adsonv04@gmail.com>
2023-10-03 22:09:26 -0300

    complemento e correção de erros parte 19 - 2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Categoria } from '@modules/catalogo/domain/categoria/categoria.entity';
+import { Produto } from '@modules/catalogo/domain/produto/produto.entity';
 import { CategoriaPrismaRepository } from '@modules/catalogo/infra/database/categoria.prisma.repository';
 import { PrismaClient } from '@prisma/client';
 import { DomainException } from '@shared/domain/domain.exception';
+
 
 const prisma = new PrismaClient({
     log: ['query', 'info'],
@@ -9,7 +11,7 @@ const prisma = new PrismaClient({
 });
 
 async function main() {
-
+    
     prisma.$connect().then(
         async () => {
             console.log('Postgres Conectado');
@@ -18,25 +20,98 @@ async function main() {
 
     const categoriaRepo = new CategoriaPrismaRepository(prisma);
 
-    const categoriaRecuperada = await categoriaRepo.recuperarPorUuid("c2666bdb-c055-40bb-951b-32f899f41e30");
+    ////////////////////////////////
+    //Recuperar Categoria por UUID//
+    ////////////////////////////////
+    
+    const categoriaRecuperada: Categoria | null = await categoriaRepo.recuperarPorUuid("7061d559-ab25-4182-98ce-170afdf2acd2");
 
     console.log(categoriaRecuperada);
 
-    const categoria: Categoria = Categoria.criar({
+    /////////////////////////////////
+    //Recuperar Todas as Categorias//
+    /////////////////////////////////
+    
+    //const todasCategorias: Array<Categoria> = await categoriaRepo.recuperarTodos();
+
+    //console.log(todasCategorias);
+
+    ////////////////////////////////
+    //Verifica se Existe Categoria//
+    ////////////////////////////////
+    
+    //const existeCategoria: boolean = await categoriaRepo.existe("7061d559-ab25-4182-98ce-170afdf2acd2");
+
+    //console.log(existeCategoria);
+
+    /////////////////////
+    //Inserir Categoria//
+    /////////////////////
+    
+    //const categoria: Categoria = Categoria.criar({
+    //    nome:'Sala e Quarto'
+    //});     
+
+    //const categoriaInserida = await categoriaRepo.inserir(categoria);
+
+    //console.log(categoriaInserida);
+
+    ///////////////////////
+    //Atualizar Categoria//
+    ///////////////////////
+    
+    //const categoria: Categoria = Categoria.recuperar({
+    //    id: "96a7f212-e01d-4de7-8abc-70cabbc898fd",
+    //    nome: "Banho"
+    //});     
+
+    //const atualizouCategoria: boolean = await categoriaRepo.atualizar(categoria.id,categoria);
+
+    //console.log(atualizouCategoria)
+
+    /////////////////////
+    //Deletar Categoria//
+    /////////////////////
+    
+    //const categoriaDeletada: boolean = await categoriaRepo.deletar("120a3d76-9ca6-4880-a1d6-d34685e1f6f8");
+    
+    //console.log(categoriaDeletada);
+
+    const produtoRepo =  new ProdutoPrismaRepository(prisma);
+
+ ////////////////////////////////
+    //Recuperar Produto por UUID//
+    ////////////////////////////////
+   
+    const produtoRecuperado: Produto | null = await produtoRepo.recuperarPorUuid("33663685-283d-4a52-b86e-7463e7077b38");
+
+    console.log(produtoRecuperado);
+
+    ///////////////////
+    //Inserir Produto//
+    ///////////////////
+
+    const categoria01: Categoria = Categoria.recuperar({
+        id: "33663685-283d-4a52-b86e-7463e7077b38",
+        nome: 'Sala'
+    });    
+
+    const categoria02: Categoria = Categoria.recuperar({
+        id: "33663685-283d-4a52-b86e-7463e7077b38",
         nome: 'Banho'
-    })
+    });
 
-    // const categoriaInserida = await categoriaRepo.inserir(categoria);
+    const produto: Produto = Produto.criar({
+        nome:'Toalha de mesa',
+        descricao:'toalha de algodão',
+        valor:40,
+        categorias:[categoria01,categoria02]
+     });
 
-    // console.log(categoriaInserida);
-    // const categorias = await categoriasRepo.inserir(categoria)
+    const produtoInserido = await produtoRepo.inserir(produto);
 
-    // console.log(categoriaInserida);
+    console.log(produtoInserido);
 
-    // const categorias = await categoriaRepo.recuperarTodos();
-    // console.log(categorias)
-
-    // const categoriaAtualizada = await categoriaRepo.atualizar();
 }
 
 main()
@@ -44,14 +119,14 @@ main()
         await prisma.$disconnect()
     })
     .catch(async (error) => {
-        if (error instanceof DomainException) {
-            console.log('Execeção de Dóminio');
-            console.log(error.message);
-        }
-        else {
-            console.log('Outras Exceções');
-            console.log(error.message);
-        }
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+       if (error instanceof DomainException) {
+           console.log('Execeção de Dóminio');
+           console.log(error.message);
+       }
+       else {
+           console.log('Outras Exceções');
+           console.log(error.message);
+       }
+       await prisma.$disconnect()
+       process.exit(1)
+   })
