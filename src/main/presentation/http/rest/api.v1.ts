3a9622b998f9eab28ef2afd08ead056ffd04fa2d
Adson Vicente <adsonv04@gmail.com>
2024-01-15 21:48:44 -0300

    15/01/2024
    Penultima parte do projeto

--- src/main/presentation/http/rest/api.v1.ts
+++ src/main/presentation/http/rest/api.v1.ts
@@ -1,6 +1,8 @@
 import express, { Router } from 'express';
 import { categoriaRouter } from '@modules/catalogo/presentation/http/rest/categoria.routes';
 import { produtoRouter } from '@modules/catalogo/presentation/http/rest/produto.routes';
+import { usuarioRouter } from '@modules/usuario/presentation/http/rest/usuario.routes';
+
 const apiv1Router: Router = express.Router();
 
 apiv1Router.use(
@@ -15,9 +17,7 @@ apiv1Router.use(
 
 apiv1Router.use(
     '/usuarios',
-    function (request, response, next) {
-        response.json({ "entidade": "Pedidos" });
-    }
+    usuarioRouter
 );
 
 apiv1Router.use(
--- src/modules/catalogo/presentation/http/rest/categoria.routes.ts
+++ src/modules/catalogo/presentation/http/rest/categoria.routes.ts
@@ -1,36 +1,41 @@
 import express from 'express';
-import { inserirCategoriaController, recuperarCategoriaPorIdController, recuperarTodasCategoriasController, atualizarCategoriaController, deletarCategoriaController } from './controllers';
+import { atualizarCategoriaController, deletarCategoriaController, inserirCategoriaController, recuperarCategoriaPorIdController, recuperarTodasCategoriasController } from './controllers';
 import { contentTypeMiddleware } from '@main/presentation/http/middlewares/content-type.middlewares.';
-import { validaInputInserirCategoria } from '../middlewares/valida-input-inserir-categoria.middlewares';
-
-
+import { authUsuario } from '@main/presentation/http/middlewares/auth-usuario.middleware'; 
+import { TipoUsuario } from '@modules/usuario/domain/usuario.types';
 const categoriaRouter = express.Router();
 
 categoriaRouter.get(
     '/:id',
-    (request, response, next) => recuperarCategoriaPorIdController.recuperar(request, response, next)
-);
+    (request, response, next) =>  recuperarCategoriaPorIdController.recuperar(request, response, next)
+)
 
 categoriaRouter.get(
     '/',
-    (request, response, next) => recuperarTodasCategoriasController.recuperar(request, response, next)
-);
+    (request, response, next) =>  recuperarTodasCategoriasController.recuperar(request, response, next)
+)
 
 categoriaRouter.post(
     '/',
-    contentTypeMiddleware,
-    validaInputInserirCategoria,
-    (request, response, next) => inserirCategoriaController.inserir(request, response, next)
-);
+     authUsuario([TipoUsuario.ADMINISTRADOR]),    contentTypeMiddleware,
+    (request, response, next) =>  inserirCategoriaController.inserir(request, response, next)
+)
 
 categoriaRouter.put(
     '/:id',
-    (request, response, next) => atualizarCategoriaController.atualizar(request, response, next)
-);
+     
+
+authUsuario([TipoUsuario.ADMINISTRADOR]),
+    contentTypeMiddleware,
+    (request, response, next) =>  atualizarCategoriaController.atualizar(request, response, next)
+)
 
 categoriaRouter.delete(
     '/:id',
-    (request, response, next) => deletarCategoriaController.deletar(request, response, next)
+    
+
+authUsuario([TipoUsuario.ADMINISTRADOR]),
+    (request, response, next) =>  deletarCategoriaController.deletar(request, response, next)
 )
 
 export { categoriaRouter };
